51de09c212c8cc6ec4345a5728145b42facd4068
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-02-03 10:54:33 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+// const findMatching = drivers.filter((driver) => driver === "Bobby");
+// function findMatching(collection) {
+//   let newArray = [];
+//   for (let item of collection) {
+//     if (item.drivers === item.drivers) return newArray.push(item.drivers);
+//   }
+// }
+
+// function findMatching(collection, name) {
+//   return collection.filter(function (el) {
+//     return el;
+//   });
+// }
+
+function findMatching(drivers, name) {
+  const result = drivers.filter(
+    (driver) => name.toLowerCase() === driver.toLowerCase()
+  );
+  return result;
+}
+
+function fuzzyMatch(drivers, name) {
+  const result = drivers.filter((driver) => driver.startsWith(name));
+  return result;
+}
+
+function matchName(drivers, string) {
+  const result = drivers.filter((driver) => driver.name === string);
+  return result;
+}
